f6b5e4179d341235809559423a12db4aaa023894
Brian Bugh <brainbag@gmail.com>
2019-08-16 14:57:28 -0500

    Quietly bundle, he's a hero

--- build-libs.sh
+++ build-libs.sh
@@ -1,16 +1,20 @@
 base_dir=$(dirname "$(realpath $0)")
 
 cd cash_flow_c
+bundle install --quiet
 rake compile
 
 cd $base_dir
 cd crates/cash_flow_rust_ffi
+bundle install --quiet
 make
 
 cd $base_dir
 cd crates/cash_flow_rust_helix
+bundle install --quiet
 rake build
 
 cd $base_dir
 cd crates/cash_flow_rust_ruru
+bundle install --quiet
 ./build-ruby.sh
